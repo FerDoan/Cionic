edd61cc9adeaef1a2b2363c2bef0f476a1ddeb30
FerDoan <fpuentes@ari.group>
2023-08-29 13:21:21 -0600

    dev

---
+++ myAppionic/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myAppionic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
